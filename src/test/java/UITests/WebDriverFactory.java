a379563f03dc53af5627d1412f6c035b789308ef
Anastasia Semisorova <semisorova29@mail.ru>
2024-11-21 15:20:49 +0300

    Diplom_3 UI Tests, new classes, fixed previous classes (annotations) and new allure report and renew class WebDriverFactory

--- src/test/java/UITests/WebDriverFactory.java
+++ src/test/java/UITests/WebDriverFactory.java
@@ -11,10 +11,14 @@ public class WebDriverFactory {
     public static WebDriver getDriver(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
-                WebDriverManager.chromedriver().setup();
+                // Автоматическое определение версии браузера и загрузка драйвера
+                //WebDriverManager.chromedriver().setup();
+                //return new ChromeDriver();
+                // Указанное определение версии браузера и загрузка драйвера
+                WebDriverManager.chromedriver().browserVersion("131.0.6778.86").setup();
                 return new ChromeDriver();
             case "yandex":
-                System.setProperty("webdriver.chrome.driver", "D:\\WebDriver\\bin\\chromedriver128.exe");
+                WebDriverManager.chromedriver().browserVersion("128").setup();
                 ChromeOptions options = new ChromeOptions();
                 options.setBinary("C:\\Users\\Anastasia\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
                 return new ChromeDriver(options);
@@ -29,5 +33,30 @@ public class WebDriverFactory {
                 throw new IllegalArgumentException("Unknown browser: " + browser);
         }
     }
+
 }
+
+// При указании конкретного пути к вебдрайверам
+    /*public static WebDriver getDriver(String browser) {
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "yandex":
+                System.setProperty("webdriver.chrome.driver", "D:\\WebDriver\\bin\\chromedriver128.exe");
+                ChromeOptions options = new ChromeOptions();
+                options.setBinary("C:\\Users\\Anastasia\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+                return new ChromeDriver(options);
+            // Для других браузеров
+            /*case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();*/
+            /*default:
+                throw new IllegalArgumentException("Unknown browser: " + browser);
+        }
+    }
+}*/
 
